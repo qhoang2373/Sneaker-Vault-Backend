9e078dcc5ed460dee549a8c2c1824638354716b0
Quan <qhoang2373@gmail.com>
2024-12-02 18:03:57 -0800

    signup route complete

--- models/user.js
+++ models/user.js
@@ -1,2 +1,22 @@
 const mongoose = require('mongoose');
 
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    hashedPassword: {
+        type: String,
+        required: true
+    },
+});
+
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        delete returnedObject.hashedPassword;
+    }
+});
+
+
+module.exports = mongoose.model('User', userSchema);   
