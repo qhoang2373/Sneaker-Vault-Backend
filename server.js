755f38a84a03e6e53f466157055dba87f6c9b426
Quan <qhoang2373@gmail.com>
2024-12-05 19:50:56 -0800

    test

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const dotenv = require('dotenv');
+const cors = require('cors');
 dotenv.config();
 const express = require('express');
 const app = express();
@@ -7,22 +8,21 @@ const testJWTRouter = require('./controllers/test-jwt');
 const usersRouter = require('./controllers/users');
 const profilesRouter = require('./controllers/profiles');
 const sneakersRouter = require('./controllers/sneakers.js');
-const cors = require('cors');
-
 
 
 mongoose.connect(process.env.MONGODB_URI);
-
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
+app.use(cors())
 app.use(express.json());
+
 app.use('/test-jwt', testJWTRouter);
 app.use('/users', usersRouter);
 app.use('/profiles', profilesRouter);
 app.use('/sneakers', sneakersRouter);
-app.use(cors())
+
 
 
 
